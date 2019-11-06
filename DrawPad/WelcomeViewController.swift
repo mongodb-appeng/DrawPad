b15645cc962e7089fa1e390b3acae9b66f9a06a0
Andrew Morgan <andrew.morgan@mongodb.com>
2019-11-06 16:59:02 +0000

    Copyright update

--- DrawPad/WelcomeViewController.swift
+++ DrawPad/WelcomeViewController.swift
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////
 //
-// Copyright 2018-2019 Realm Inc.
+// Copyright 2019 MongoDB Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
