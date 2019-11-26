b9ab0b5cad3a5a5961aabe88aa2c4e12587e635a
Diana Esteves <diana.esteves@mongodb.com>
2019-11-25 22:04:38 -0500

    quick commit on Mon Nov 25 22:04:38 EST 2019

---
+++ DrawPad/TermConditionsPrivacyPolicyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TermConditionsPrivacyPolicyViewController.swift
+//  DrawPad
+//
+//  Created by Diana Esteves on 11/25/19.
+//  Copyright © 2019 MongoDB Inc. All rights reserved.
+//
+
+
+import UIKit
+
+class TermConditionsPrivacyPolicyViewController: UIViewController {
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true) {          }
+    }
+    
+    @IBAction func exit(_ sender: Any) {
+        dismiss(animated: true) {          }
+    }
+}
